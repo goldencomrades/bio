b1463873e9b735dea1577178f9293a3295394277
francesco <info.francescobianco@gmail.com>
2019-04-01 03:29:25 +0200

    added 3d graph taxonomy

---
+++ index.js
@@ -0,0 +1,17 @@
+const Graph = ForceGraph3D()
+(document.getElementById("3d-graph"));
+
+let curDataSetIdx;
+const dataSets = getGraphDataSets();
+
+let toggleData;
+(toggleData = function() {
+    curDataSetIdx = curDataSetIdx === undefined ? 0 : (curDataSetIdx+1)%dataSets.length;
+    const dataSet = dataSets[curDataSetIdx];
+
+    Graph.resetProps(); // Wipe current state
+    Graph.backgroundColor('#FFFFFF');
+    dataSet(Graph); // Load data set
+
+    document.getElementById('graph-data-description').innerHTML = dataSet.description ? `Viewing ${dataSet.description}` : '';
+})(); // IIFE init
